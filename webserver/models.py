6949ffaf9af06cf48a83ebcbccd923dbeb9624da
Wim Bonekamp <wim@wibosa.nl>
2022-06-02 17:55:55 +0200

    moving old users table into new user table

--- webserver/models.py
+++ webserver/models.py
@@ -1,8 +1,8 @@
 from app import db
 from flask_login import UserMixin
 
-class User(UserMixin, db.Model):
-    __tablename__ = "user"
+class Users(UserMixin, db.Model):
+    __tablename__ = "users"
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -12,14 +12,14 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-class Users(UserMixin, db.Model):
-    __tablename__ = "users"
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
     
-    user_id  = db.Column(db.Integer, primary_key=True)
+    id  = db.Column(db.Integer, primary_key=True)
     name     = db.Column(db.String(80), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email    = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(300), nullable=False, unique=False)
+    pwd = db.Column(db.String(300), nullable=False, unique=False)
     pict_file= db.Column(db.String(300), nullable=False, unique=False)
 
     def __repr__(self):
